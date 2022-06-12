c8268b350d33e29d9227ff7bd4c832d5bf4bdb47
Ellie-exe <ryiscool247@gmail.com>
2022-06-12 19:20:41 -0400

    Added deck generation and shuffling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
+struct Card {
+    rank: i8,
+    suit: i8
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut deck: Vec<Card> = Vec::new();
+
+    for suit in 0..4 {
+        for rank in 0..13 {
+            let card = Card {
+                rank,
+                suit
+            };
+
+            deck.push(card);
+        }
+    }
+
+    deck.shuffle(&mut thread_rng());
 }
